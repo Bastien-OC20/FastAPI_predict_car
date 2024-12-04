0a1c532d99defced9fd2d988797f85aa68862702
Sebastien <bastien.rapuzzi@gmail.com>
2024-12-04 12:53:53 +0100

    :bug: [database.py] fix a bug

--- database.py
+++ database.py
@@ -17,11 +17,3 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-
-# Fonction pour obtenir une session de base de données
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
