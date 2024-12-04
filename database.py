203fc29b560ac4e92fb17e28fa10e5d5c8966be6
Sebastien <bastien.rapuzzi@gmail.com>
2024-12-04 17:42:10 +0100

    :bug: [multi file]

--- database.py
+++ database.py
@@ -17,3 +17,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+# Fonction pour obtenir une session de base de données
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
