d1f9cb524ff84f75a0d7c052fd0cf0a9269cf194
Sebastien <bastien.rapuzzi@gmail.com>
2024-12-04 08:34:17 +0100

    :bug: [main.py] add new callback

--- main.py
+++ main.py
@@ -175,3 +175,9 @@ def login_for_access_token(
 @app.get("/users/me", response_model=schemas.User)
 def read_users_me(current_user: schemas.User = Depends(get_current_user)):
     return current_user
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
